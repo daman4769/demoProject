b7240951a07bd79db6f400b27befdf76eb522364
daman4769 <daman476@gmail.com>
2024-12-23 20:52:08 +0530

    third commit

--- src/com/aman/git/push.java
+++ src/com/aman/git/push.java
@@ -5,7 +5,9 @@ public class push {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
            System.out.println("Aman");
-           
+           for(int i =0 ; i<=10; i++) {
+        	   System.out.println(i);
+           }
 	}
 
 }
